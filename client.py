db1878033d44a76e520367b723b2c0271e8837cd
LebranJames02 <2294517714@qq.com>
2025-05-08 19:34:32 +0800

    Usage: python client.py <hostname> <port> <request_file

--- client.py
+++ client.py
@@ -7,3 +7,11 @@ def send_request(client_socket, request):
     # 接收响应消息
     response = client_socket.recv(1024).decode()
     return response
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python client.py <hostname> <port> <request_file>")
+        sys.exit(1)
+
+    hostname = sys.argv[1]
+    port = int(sys.argv[2])
+    request_file = sys.argv[3]
