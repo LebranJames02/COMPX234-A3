6a0bff1c570c003ef261477eb3d8a7f0a690c1d5
LebranJames02 <2294517714@qq.com>
2025-05-08 19:46:17 +0800

    handle_client

--- sever.py
+++ sever.py
@@ -63,3 +63,44 @@ class TupleSpace:
                 f"Errors: {self.error_count}"
             )
             return summary
+
+    def handle_client(client_socket, tuple_space):
+        tuple_space.client_count += 1
+        while True:
+            try:
+                request = client_socket.recv(1024).decode()
+                if not request:
+                    break
+                size = int(request[:3])
+                operation = request[4]
+                key = request[6:size - 1] if operation != 'P' else request[6:size - len(request.split()[-1]) - 2]
+                value = request[size - len(request.split()[-1]) - 1:] if operation == 'P' else ""
+
+                tuple_space.operation_count += 1
+                if operation == 'P':
+                    result = tuple_space.put(key, value)
+                    if result == 0:
+                        response = f"{str(len(f'OK ({key}, {value}) added') + 3).zfill(3)} OK ({key}, {value}) added"
+                    else:
+                        response = f"{str(len(f'ERR {key} already exists') + 3).zfill(3)} ERR {key} already exists"
+                elif operation == 'R':
+                    result = tuple_space.read(key)
+                    if result:
+                        response = f"{str(len(f'OK ({key}, {result}) read') + 3).zfill(3)} OK ({key}, {result}) read"
+                    else:
+                        response = f"{str(len(f'ERR {key} does not exist') + 3).zfill(3)} ERR {key} does not exist"
+                elif operation == 'G':
+                    result = tuple_space.get(key)
+                    if result:
+                        response = f"{str(len(f'OK ({key}, {result}) removed') + 3).zfill(3)} OK ({key}, {result}) removed"
+                    else:
+                        response = f"{str(len(f'ERR {key} does not exist') + 3).zfill(3)} ERR {key} does not exist"
+                else:
+                    response = f"{str(len('ERR invalid operation') + 3).zfill(3)} ERR invalid operation"
+                    tuple_space.error_count += 1
+
+                client_socket.send(response.encode())
+            except Exception as e:
+                print(f"Error handling client: {e}")
+                break
+        client_socket.close()
